363d9cf75ccbb342853d0c0c479f6e3ca2400688
benedictJHGil <benedictJHGil@gmail.com>
2021-08-21 17:40:33 +0900

    Modify about.js

--- src/store/about.js
+++ src/store/about.js
@@ -3,7 +3,7 @@ export default {
   state: () => ({
     name: 'benedictJHGil',
     email: 'benedictJHGil@gmail.com',
-    phone: '+82-10-1234-5678',
+    phone: '+82-10-5424-2374',
     image: 'https://raw.githubusercontent.com/benedictJHGil/vue3-movie-app/master/src/assets/logo.png'
     // image: 'https://heropy.blog/css/images/logo.png'
   })
