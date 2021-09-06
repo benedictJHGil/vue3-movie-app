185dcd39f1487ed31483a8ff95af3c8e74c97164
benedictJHGil <benedictJHGil@gmail.com>
2021-09-06 10:18:02 +0900

    Intiate unit-test

--- src/store/movie.js
+++ src/store/movie.js
@@ -110,7 +110,10 @@ export default {
                     loading: false
                 })
             }
-        }
+        },
+        // sampleAction(context, payload) {
+        //     const { state, getters, commit, dispatch } = context
+        // }
     }
 }
 
